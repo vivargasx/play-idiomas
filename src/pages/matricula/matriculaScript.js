e5e8701e70cfde43bd37b2ebe0970d185ab4c370
Vitor Vargas <vitorvargasbh@gmail.com>
2025-02-05 22:25:02 -0300

    Atualizando formatação formulario

--- src/pages/matricula/matriculaScript.js
+++ src/pages/matricula/matriculaScript.js
@@ -8,3 +8,17 @@ document.getElementById("formMatricula").addEventListener("submit", function(eve
     
     this.reset();
 });
+
+
+document.getElementById("telefone").addEventListener("input", function(event) {
+    let value = this.value.replace(/\D/g, "");
+
+    if (value.length > 2) {
+        value = `(${value.slice(0, 2)}) ${value.slice(2)}`;
+    }
+    if (value.length > 10) {
+        value = `${value.slice(0, 10)}-${value.slice(10, 15)}`;
+    }
+
+    this.value = value.slice(0, 15); 
+});
